dfe077c89db00317023f5c4ceaf9c4f547fd26c3
Hien Nguyen <hiennguyen@Hiens-MacBook-Air.local>
2021-09-15 11:08:14 -0700

    2nd changes

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,20 +1,25 @@
 const {merge} = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
-const { module } = require('./webpack.common');
+const packageJson = require('../package.json');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const domain = process.env.PRODUCTION_DOMAIN;
 
 const devConfig = {
     mode: 'production',
-    devServer: {
-        port: 8081,
-        historyApiFallback: {
-            index: 'index.html'
-        }
+    output: {
+        filename: '[name].[contenthash].js'
     },
-    plugin: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
+    plugins: [
+        new ModuleFederationPlugin(
+            {
+                name: 'container',
+                remotes: {
+                    marketing: `marketing@${domain}/marketing/remoteEntry.js`
+                },
+                shared: packageJson.dependencies
+            }
+        ),
     ]
 }
 
